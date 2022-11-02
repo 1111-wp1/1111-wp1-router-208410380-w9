845582acbfc5c04d744f634927880e7a83a2d581
justin40715 <justin40715@gmail.com>
2022-11-02 20:56:03 +0800

    w09-P3: find a product using productId using your last digit of ID

--- w09_router/src/App_80.js
+++ w09_router/src/App_80.js
@@ -4,6 +4,8 @@ import AboutPage_80 from './pages/AboutPage_80';
 import ProductPage_80 from './pages/ProductsPage_80';
 import ErrorPage_80 from './pages/ErrorPage_80';
 import SharedLayout_80 from './pages/SharedLayout_80';
+import SharedProductLayout_80 from './pages/SharedProductLayout_80';
+import SingleProductPage_80 from './pages/SingleProductPage_80';
 
 function App() {
   return (
@@ -12,7 +14,10 @@ function App() {
         <Route path='/' element={<SharedLayout_80 />}>
           <Route index element={<HomePage_80 />} />
           <Route path='about' element={<AboutPage_80 />} />
-          <Route path='products' element={<ProductPage_80 />} />
+          <Route path='products' element={<SharedProductLayout_80 />}>
+            <Route index element={<ProductPage_80 />} />
+            <Route path=':productId' element={<SingleProductPage_80 />} />
+          </Route>
           <Route path='*' element={<ErrorPage_80 />} />
         </Route>
       </Routes>
